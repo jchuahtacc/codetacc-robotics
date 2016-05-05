19bff53e8b6414e82abebd5b9b2269865c66eae4
Joon Yee Chuah <jchuah@tacc.utexas.edu>
2016-05-05 13:59:21 -0500

    Initial Push

---
+++ README.md
@@ -0,0 +1,16 @@
+# CODE @ TACC Robotics library
+
+### Introduction
+
+This library wraps functionality for several Adafruit Arduino shield libraries,
+as well as provides general compatibility to these libraries. Included is an
+example ```robot.ino``` file, to be used as a project base for the
+CODE @ TACC Summer 2016 curriculum. Additional
+examples may be included for operation of sensors.
+
+### CODE @ TACC
+
+CODE @ TACC is a summer residential program for rising juniors and seniors
+in high school, hosted by the Texas Advanced Computing Center at
+The University of Texas at Austin. [You may view the open source curriculum
+on GitHub.](https://github.com/CODE-at-TACC/summer-2016)
---
+++ firmware/examples/robot.ino
@@ -0,0 +1,76 @@
+/******************************************
+
+Included libraries
+
+******************************************/
+#include <codetacc-robotics.h>
+
+/******************************************
+
+Variables
+
+******************************************/
+
+Adafruit_MotorShield shield = Adafruit_MotorShield();
+Adafruit_DCMotor *leftMotor = shield.getMotor(1);
+Adafruit_DCMotor *rightMotor = shield.getMotor(2);
+
+/******************************************
+
+Custom functions
+
+******************************************/
+
+int forward(String params = "") {
+  leftMotor->run(FORWARD);
+  rightMotor->run(FORWARD);
+  return 1;
+}
+
+int left(String params = "") {
+  leftMotor->run(BACKWARD);
+  rightMotor->run(FORWARD);
+  return 1;
+}
+
+int right(String params = "") {
+  leftMotor->run(FORWARD);
+  rightMotor->run(BACKWARD);
+  return 1;
+}
+
+int stop(String params = "") {
+  leftMotor->run(RELEASE);
+  rightMotor->run(RELEASE);
+  return 1;
+}
+
+
+/******************************************
+void setup()
+
+Runs once upon startup
+******************************************/
+
+void setup() {
+  shield.begin();
+  leftMotor->setSpeed(150);
+  rightMotor->setSpeed(150);
+}
+
+/******************************************
+void loop()
+
+Runs forever
+******************************************/
+
+void loop() {
+  forward();
+  delay(1000);
+  stop();
+  delay(1000);
+  left();
+  delay(1000);
+  right();
+  delay(1000);
+}
